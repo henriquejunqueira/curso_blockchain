553543f3f9f50389e181ea0a1613d685cacd24f7
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-02-14 15:40:12 -0300

    Adicionando blocos na blockchain via post request

--- app/index.js
+++ app/index.js
@@ -5,12 +5,21 @@ const HTTP_PORT = process.env.HTTP_PORT || 3001;
 const app = express();
 const bc = new Blockchain();
 
+app.use(express.json());
+
+// Return the blocks
 app.get('/blocks', (req, res) => {
   res.json(bc.chain);
 });
 
+// Add blocks to the Blockchain
+app.post('/mine', (req, res) => {
+  const block = bc.addBlock(req.body.data);
+  console.log(`New Block added: ${block.toString()}`);
+
+  res.redirect('/blocks');
+});
+
 app.listen(HTTP_PORT, () =>
-  console.log(
-    `Listening on port ${HTTP_PORT}. Access via link: http://localhost:${HTTP_PORT}`
-  )
+  console.log(`Access via link: http://localhost:${HTTP_PORT}`)
 );
