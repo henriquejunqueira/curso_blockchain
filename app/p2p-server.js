e60e4e3fdc602df361f3762a74fc4e3baf7e84b3
henriquejunqueira <henriquejunqueira15@gmail.com>
2025-02-17 14:29:06 -0300

    Started creating the P2pServer class

--- app/p2p-server.js
+++ app/p2p-server.js
@@ -1,1 +1,15 @@
 const Websocket = require('ws');
+
+const P2P_PORT = process.env.P2P_PORT || 5001;
+const peers = process.env.PEERS ? process.env.PEERS.split(',') : [];
+
+// $ HTTP_PORT = 3002 P2P_PORT = 5003 PEERS=ws://localhost:5001, ws://localhost:5002 npm run dev
+
+// A classe P2pServer é usada para criar um servidor P2P (peer-to-peer) que se conecta a outros
+// nós na rede e permite a sincronização de dados.
+class P2pServer {
+  constructor(blockchain) {
+    this.blockchain = blockchain;
+    this.socket = [];
+  }
+}
